97b55391d7033310c18948ed40a21c348a487bfa
Pavel Didenko <patriot2006_06@mail.ru>
2024-01-22 20:46:00 +0300

    feat: align button finally

--- src/components/PictureCard/PictureCard.tsx
+++ src/components/PictureCard/PictureCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import SvgIcon from "@mui/material/SvgIcon";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
@@ -14,16 +14,19 @@ export function PictureCard({ url, likedByUser, likes }: PictureCardProps) {
   return (
     <Box sx={{ marginTop: "2rem" }}>
       <Image src={url} alt="picture" width="360" height="300" />
-      <Box sx={{ display: "flex" }}>
-        <SvgIcon sx={{ cursor: "pointer" }}>
-          {!likedByUser ? (
-            <FavoriteBorderIcon />
-          ) : (
-            <FavoriteIcon sx={{ color: "red" }} />
-          )}
-        </SvgIcon>
-        <Typography>{likes}</Typography>
-        <Typography component="pre"> Likes</Typography>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={{ display: "flex" }}>
+          <SvgIcon sx={{ cursor: "pointer" }}>
+            {!likedByUser ? (
+              <FavoriteBorderIcon />
+            ) : (
+              <FavoriteIcon sx={{ color: "red" }} />
+            )}
+          </SvgIcon>
+          <Typography>{likes}</Typography>
+          <Typography component="p"> Likes</Typography>
+        </Box>
+        <Button variant="outlined">View picture</Button>
       </Box>
     </Box>
   );
